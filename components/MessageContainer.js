a462b935dd296b461d7450dc5c1dda1e1b7e0d85
Jen Beckett <jennybeckett2@hotmail.com>
2022-02-02 14:05:22 -0800

    Added in the new message component to the app. Console.lgo reflects the data being entered, but need to add state to the page.

--- components/MessageContainer.js
+++ components/MessageContainer.js
@@ -1,11 +1,32 @@
 import React from "react";
 import MessageTable from "./MessageTable";
+import NewMesssageForm from "./NewMessageForm";
+
+const  MessageContainer = () => {
+    const messages = [];
+    messages[0] = {"id": 0, "name": "Avril Lavigne", "message": "He was a skaterboi...."};
+    messages[1] = {"id": 1, "name": "Betty White", "message": "Animals > Humans"};
+    messages[2] = {"id": 2, "name": "Rick Sanchez", "message": "Wubba Lubba Dub Dub!!"};
+    messages[3] = {"id": 3, "name": "The_Real_Meatloaf (RIP)", "message": "Who am I, why am I here? Forget the question, someone give me another beer."};
+    messages[4] = {"id": 4, "name": "Hank Hill", "message": "Goddamnit Bobby...."};
+    messages[5] = {"id": 5, "name": "Nicolas Cage in Ghost Rider: Spirit Of Vengeance ", "message": "I'm a good actor I promise plz don't judge me on this preformance alone"};
 
 
-const  MessageContainer = () => (
+    // function called by Formik to pass data from the Form
+    const addNewMessage = (values) => {
+        console.log(values);
+        values.id = messages.length;
+        messages.unshift(values);
+        console.log(messages);
+    }
 
-        <MessageTable/>
-    );
+    return(
+        <>
+            <NewMesssageForm addNewMessage={addNewMessage} />
+            <MessageTable messages={messages}/>
+        </>
+        )
+}
 
 
 export default MessageContainer;
--- components/MessageTable.js
+++ components/MessageTable.js
@@ -2,18 +2,8 @@ import React from "react";
 import MessageTableRow from "./MessageTableRow";
 import Table from 'react-bootstrap/Table';
 
-const  MessageTable = () => {
-        const messages = [];
-        messages[0] = {"id": 0, "name": "Avril Lavigne", "message": "He was a skaterboi...."};
-        messages[1] = {"id": 1, "name": "Betty White", "message": "Animals > Humans"};
-        messages[2] = {"id": 2, "name": "Rick Sanchez", "message": "Wubba Lubba Dub Dub!!"};
-        messages[3] = {"id": 3, "name": "The_Real_Meatloaf (RIP)", "message": "Who am I, why am I here? Forget the question, someone give me another beer."};
-        messages[4] = {"id": 4, "name": "Hank Hill", "message": "Goddamnit Bobby...."};
-        messages[5] = {"id": 5, "name": "Nicolas Cage in Ghost Rider: Spirit Of Vengeance ", "message": "I'm a good actor I promise plz don't judge me on this preformance alone"};
-
-
-
-        //const { Table } = ReactBootstrap;
+const  MessageTable = ({messages}) => {
+       
         return (
 
             <div className="m-3 p-4-dark ">
